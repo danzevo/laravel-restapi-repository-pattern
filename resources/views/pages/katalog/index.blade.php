401f041b3221b7bce27cefe3f5a9795e11483b20
dani nugrahadi pradana <dani.nugrahadi@gmail.com>
2022-08-31 19:04:52 +0700

    fix katalog produk & fix display image if no data

--- resources/views/pages/katalog/index.blade.php
+++ resources/views/pages/katalog/index.blade.php
@@ -3,26 +3,32 @@
 @section('title', 'Produk')
 
 @section('content_header')
-    <h1>Produk</h1>
+    <h1>Katalog Produk</h1>
 @stop
 
 @section('content')
 <div class="row">
-    @foreach($product as $row)
-        <div class="col-lg-3 d-flex align-items-stretch">
-            <div class="card mb-3">
-                <img style="padding:25px" src="{{ asset('image_product/'.$row->image) }}" class="card-img-top" alt="{{ $row->image }}">
-                <div class="card-body">
-                    <p class="text-center"><strong>{{ $row->nama }}</strong></p>
-                    <p class="text-center"><strong>{{ 'Rp. '.number_format($row->harga, 0, ',', '.') }}</strong></p>
-                    <p class="card-text">{!! $row->deskripsi !!}</p>
-                </div>
-                <div class="card-footer">
-                    <p class="text-center"><button class="btn btn-warning">Beli</button></p>
+    @if(count($product) > 0)
+        @foreach($product as $row)
+            <div class="col-lg-3 d-flex align-items-stretch">
+                <div class="card mb-3">
+                    <img style="padding:25px" src="{{ asset('image_product/'.$row->image) }}" class="card-img-top" alt="{{ $row->image }}">
+                    <div class="card-body">
+                        <p class="text-center"><strong>{{ $row->nama }}</strong></p>
+                        <p class="text-center"><strong>{{ 'Rp. '.number_format($row->harga, 0, ',', '.') }}</strong></p>
+                        <p class="card-text">{!! $row->deskripsi !!}</p>
+                    </div>
+                    <div class="card-footer">
+                        <p class="text-center"><button class="btn btn-warning">Beli</button></p>
+                    </div>
                 </div>
             </div>
+        @endforeach
+    @else
+        <div class="col-lg-3 d-flex align-items-stretch">
+            <p>Data not Found</p>
         </div>
-    @endforeach
+    @endif
 </div>
 @stop
 
